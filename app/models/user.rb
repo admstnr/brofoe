52d40100dcc9560a4d6b1ee260cd98ed985cf8d8
Cam Crews <cam@nycda.com>
2015-07-14 16:46:03 -0400

    bcrypt password fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest) == password
+  end
+
   has_many :user_groups
   has_many :groups, through: :user_groups
   has_many :favorites
@@ -9,4 +18,5 @@ class User < ActiveRecord::Base
   scope :new_users, -> {
     where('created_at between ? and ?',Time.now-1.day,Time.now)
   }
+  
 end
