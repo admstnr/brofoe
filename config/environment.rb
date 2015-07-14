ec6add23c9bd5d9c67492efbb2188809cdc8fc78
Cam Crews <cam@nycda.com>
2015-07-14 10:48:58 -0400

    Database config pg for production

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# set environment to production, if not set locally
+ENV['RAILS_ENV'] ||= 'production'
